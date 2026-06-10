a7ba97ee9a8c28a5325ebc8959fcc9d83b6e2d10
ReddySagar18 <reddysagar200716@gmail.com>
2026-06-10 14:30:41 +0530

    commit

--- database.py
+++ database.py
@@ -1,23 +1,17 @@
+import os
 import psycopg2
 import psycopg2.extras
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
-    )
+DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_EWZy4GKFO3NM@ep-delicate-fire-ap6fsw70-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
+def get_db_connection():
+    if not DATABASE_URL:
+        raise RuntimeError("DATABASE_URL is not set. Check your Railway/Neon env vars.")
+    conn = psycopg2.connect(DATABASE_URL)
     return conn
 
-
 def get_dict_cursor(conn):
-    return conn.cursor(
-        cursor_factory=psycopg2.extras.RealDictCursor
-    )
+    return conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
