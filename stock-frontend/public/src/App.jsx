b0d33b80c1802e3d35894d4e253c3aa02499c2f5
ReddySagar18 <reddysagar200716@gmail.com>
2026-06-20 15:02:59 +0530

    testing

--- stock-frontend/public/src/App.jsx
+++ stock-frontend/public/src/App.jsx
@@ -1,11 +1,6 @@
 import SplashScreen from "./components/SplashScreen";
-import { useState } from "react";
 
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
 
-import "./styles/dashboard.css";
 
 function App() {
  return <SplashScreen />;
